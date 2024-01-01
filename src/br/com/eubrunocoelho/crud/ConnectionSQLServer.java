f9c0bb86150d636d247bc472320feb904459bfb0
eubrunocoelho <eu.brunocoelho94@gmail.com>
2024-01-01 14:06:13 -0200

    feat: establishes connection to the database

--- src/br/com/eubrunocoelho/crud/ConnectionSQLServer.java
+++ src/br/com/eubrunocoelho/crud/ConnectionSQLServer.java
@@ -1,9 +1,19 @@
 package br.com.eubrunocoelho.crud;
 
-public class ConnectionSQLServer {
-    public static String getConnection() {
-        var Url = Configuration.getValue("db.Url");
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
-        return Url;
+public class ConnectionSQLServer {
+    public static void getConnection() {
+        var url = Configuration.getValue("db.Url");
+        var user = Configuration.getValue("db.User");
+        var password = Configuration.getValue("db.Password");
+
+        try (Connection connection = DriverManager.getConnection(url, user, password)) {
+            System.out.println("Conexão estabelecida com sucesso!");
+        } catch (SQLException e) {
+            System.err.println("Erro ao estabelecer conexão com o banco de dados: " + e.getMessage());
+        }
     }
 }
